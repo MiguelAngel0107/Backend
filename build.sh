acd998720e95f0e750f9c59662e1ae034c0a6979
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-01-12 21:07:57 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python core/manage.py collectstatic --no-input
+python manage.py collectstatic --no-input
 
-python core/manage.py migrate
+python manage.py migrate
