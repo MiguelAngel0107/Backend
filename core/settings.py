dd2bbc8bf7604700206c6d756d4383dcf2143941
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-01-12 21:14:11 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -75,14 +75,14 @@ CKEDITOR_CONFIGS = {
 CKEDITOR_UPLOAD_PATH = "/media/"
 
 MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+
     'social_django.middleware.SocialAuthExceptionMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
 
-    'corsheaders.middleware.CorsMiddleware',
-
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
