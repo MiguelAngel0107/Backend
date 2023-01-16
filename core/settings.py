ef14879cc57cba8ca2c7405dbb01c2729f6d7ae6
MiguelAngel0107 <95198651+MiguelAngel0107@users.noreply.github.com>
2023-01-15 21:56:38 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -299,12 +299,31 @@ if DEBUG and TESTOFS3:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
 
 
+    
+    TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [os.path.join(BASE_DIR, 'build')],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+
 if DEBUG and not TESTOFS3:
-    MEDIA_URL = '/media/'
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'build/static'),)
+
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
